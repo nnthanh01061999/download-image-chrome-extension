8ea7829bfe3c29e52cd9aedf2d1e12834e7c408a
nnthanh01061999 <nnthanh01061999@gmail.com>
2023-05-21 23:35:48 +0700

    init extension

---
+++ react-chrome-app/src/App.tsx
@@ -0,0 +1,12 @@
+function App() {
+  return (
+    <div>
+      <header>
+        <h5>Download Image</h5>
+      </header>
+      <body>Download Image ne ban</body>
+    </div>
+  );
+}
+
+export default App;
---
+++ react-chrome-app/src/index.tsx
@@ -0,0 +1,39 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+
+const rootElement = document.createElement("div");
+rootElement.id = "react-chrome-app";
+
+const globalStyles = document.createElement("style");
+globalStyles.innerHTML = `
+  #${rootElement.id} {
+  position: fixed;
+  right: 0;
+  top: 0;
+  width: 500px;
+  height: 100vh;
+  background: #ffffff;
+  border-right: 1px solid #c2c2c2;
+  z-index: 999;
+  font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', 'Roboto', 'Oxygen','Ubuntu', 'Cantarell', 'Fira Sans', 'Droid Sans', 'Helvetica Neue',sans-serif;
+  margin: 0;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+}
+`;
+rootElement.appendChild(globalStyles);
+document.body.appendChild(rootElement);
+
+const root = ReactDOM.createRoot(rootElement);
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
